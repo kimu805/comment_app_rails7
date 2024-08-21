08db894dac02c03e8bd5d1e302a75e2bff76b270
kimu805 <kimukimuok805@gmail.com>
2024-08-21 18:25:54 +0900

    set up channel and send data to js

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,7 +3,7 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     if @comment.save
-      redirect_to item_path(params[:item_id])
+      ActionCable.server.broadcast "comment_channel", {comment: @comment, user: @comment.user}
     end
   end
 
